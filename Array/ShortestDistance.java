82010a9164f2b1e5904a9b50c7a8fa542848b652
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-27 19:27:31 -0400

    Update ShortestDistance.java

--- Array/ShortestDistance.java
+++ Array/ShortestDistance.java
@@ -1,15 +1,17 @@
 //P.243
-public class ShortestDistance {
+public class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
-        int pos1 = -1;
+        int pos1 = -1, pos2 = -1;
         int res = words.length;
-        for (int i = 0; i < word.length; i++) {
+        for (int i = 0; i < words.length; i++) {
             if (words[i].equals(word1)) {
                 pos1 = i;
-            } else if (pos1 != -1 && words[i].equals(word2)) {
-                if (res > Math.abs(pos1 - i)) {
-                    res = Math.abs(pos1 - i);
-                }
+            } 
+            if (words[i].equals(word2)) {
+                pos2 = i;
+            } 
+            if (Math.abs(pos1 - pos2) < res) {
+                res = Math.abs(pos1 - pos2);
             }
         }
         return res;
