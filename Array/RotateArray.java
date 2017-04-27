fdeafe4fab1222073fd8438f3928d2ffdae6b2de
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-27 16:57:16 -0400

    mod k with length

--- Array/RotateArray.java
+++ Array/RotateArray.java
@@ -1,5 +1,6 @@
 public class RotateArray {
     public void rotate(int[] nums, int k) {
+        k %= nums.length;
         reverse(nums, 0, nums.length);
         reverse(nums, 0, k);
         reverse(nums, k, nums.length);
