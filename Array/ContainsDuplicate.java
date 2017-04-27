5510296cad85a9100efc07268bb7cec54d3d9128
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-27 14:50:20 -0400

    Create ContainsDuplicate.java

---
+++ Array/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (!set.add(n)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
