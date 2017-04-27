d36de09befe2e19a4723fa80aafda8d4fc779d30
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-27 16:56:24 -0400

    Create RotateArray.java

---
+++ Array/RotateArray.java
@@ -0,0 +1,14 @@
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        reverse(nums, 0, nums.length);
+        reverse(nums, 0, k);
+        reverse(nums, k, nums.length);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        for (int i = start, j = end - 1; i < j; i++, j--) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+}
