56469589d9821958329fc0e996908699322a253c
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-29 00:22:37 -0400

    Create MaxArea.java

---
+++ Two Pointers/MaxArea.java
@@ -0,0 +1,18 @@
+public class MaxArea {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int area = 0;
+        while (i < j) {
+            int temp = 0;
+            if (height[i] < height[j]) {
+                temp = height[i] * (j - i);
+                i++;
+            } else {
+                temp = height[j] * (j - i);
+                j--;
+            }
+            area = Math.max(temp, area);
+        }
+        return area;
+    }
+}
