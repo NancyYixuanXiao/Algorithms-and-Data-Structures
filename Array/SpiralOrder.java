fe80a49e2bfe77f880044c1c1cd27391602acce2
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-30 00:26:11 -0400

    Create SpiralOrder.java

---
+++ Array/SpiralOrder.java
@@ -0,0 +1,35 @@
+public class SpiralOrder {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return res;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+            // after top++ and right--, need to make sure the conditions still hold
+            if (top <= bottom && left <= right) {
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (top <= bottom && left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
