26852b76f50b1a1c1ef5f69910839a004e3349d9
NancyYixuanXiao <yxxiao@bu.edu>
2017-04-30 00:44:07 -0400

    Create MergeInterval.java

---
+++ Array/MergeInterval.java
@@ -0,0 +1,23 @@
+//P.56
+public class MergeInterval {
+    public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> res = new ArrayList<>();
+        if (intervals == null || intervals.size() == 0) {
+            return res;
+        }
+        Collections.sort(intervals, (Interval inv1, Interval inv2) -> (inv1.start - inv2.start));
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals.get(i).start <= end) {
+                end = Math.max(end, intervals.get(i).end);
+            } else {
+                res.add(new Interval(start, end));
+                start = intervals.get(i).start;
+                end = intervals.get(i).end;
+            }
+        }
+        res.add(new Interval(start, end));
+        return res;
+    }
+}
