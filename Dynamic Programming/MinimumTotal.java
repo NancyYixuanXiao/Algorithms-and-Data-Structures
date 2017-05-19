8ec4df710fce75b32f7f63fe90250284f6e1d220
NancyYixuanXiao <yxxiao@bu.edu>
2017-05-19 10:00:16 -0400

    Create MinimumTotal.java

---
+++ Dynamic Programming/MinimumTotal.java
@@ -0,0 +1,17 @@
+//P.120
+
+public class MinimumTotal {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        int n = triangle.size();
+        int[] f = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                f[j] = Math.min(f[j], f[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return f[0];
+    }
+}
