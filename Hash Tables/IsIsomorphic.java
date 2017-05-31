313b6b14313efbac491813ec9d0c63416c06209f
NancyYixuanXiao <yxxiao@bu.edu>
2017-05-31 15:37:39 -0400

    Create IsIsomorphic.java

---
+++ Hash Tables/IsIsomorphic.java
@@ -0,0 +1,19 @@
+public class IsIsomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (map.get(s.charAt(i)) != t.charAt(i)) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
