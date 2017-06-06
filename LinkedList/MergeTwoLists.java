4a57f403833d7c3d64db6f14b7a3e658c9ace610
NancyYixuanXiao <yxxiao@bu.edu>
2017-06-06 12:17:24 -0400

    Update MergeTwoLists.java

--- LinkedList/MergeTwoLists.java
+++ LinkedList/MergeTwoLists.java
@@ -7,6 +7,7 @@
  * }
  */
 public class MergeTwoLists {
+    // iteratively
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode i = l1, j = l2;
         ListNode dummy = new ListNode(0);
@@ -28,5 +29,24 @@ public class MergeTwoLists {
             prev.next = j;
         }
         return dummy.next;
+    }
+    
+    // revursively
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            ListNode next = l1.next;
+            l1.next = mergeTwoLists(next, l2);
+            return l1;
+        } else {
+            ListNode next = l2.next;
+            l2.next = mergeTwoLists(l1, next);
+            return l2;
+        }
     }
 }
