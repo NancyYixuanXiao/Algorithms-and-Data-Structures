269b4123645bab38e392d837698c13104cc5fe01
NancyYixuanXiao <yxxiao@bu.edu>
2017-06-13 17:52:51 -0400

    Update NumDecodings.java

--- Dynamic Programming/NumDecodings.java
+++ Dynamic Programming/NumDecodings.java
@@ -1,16 +1,22 @@
-public class NumDecodings {
+public class Solution {
     public int numDecodings(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
         int[] dp = new int[s.length() + 1];
         dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 1; i < s.length(); i++) {
-            if (Integer.parseInt(s.substring(i - 1, i + 1)) < 27) {
-                dp[i + 1] = dp[i] + dp[i - 1];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                if (i == 0 || s.charAt(i - 1) > '2' || s.charAt(i - 1) == '0') {
+                    return 0;
+                } 
+                dp[i + 1] = dp[i - 1];
             } else {
-                dp[i + 1] = dp[i];
+                if (i > 0 && Integer.parseInt(s.substring(i - 1, i + 1)) < 27 && s.charAt(i - 1) != '0') {
+                    dp[i + 1] = dp[i] + dp[i - 1];
+                } else {
+                    dp[i + 1] = dp[i];
+                }
             }
         }
         return dp[s.length()];
