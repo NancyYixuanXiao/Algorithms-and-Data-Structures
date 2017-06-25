e7e759da23a776a36ea0119e9cabad70f4230c47
NancyYixuanXiao <yxxiao@bu.edu>
2017-06-25 15:19:47 -0400

    Update HasPathSum.java

--- Tree/HasPathSum.java
+++ Tree/HasPathSum.java
@@ -7,6 +7,8 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+// Solution 1
 public class HasPathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) {
@@ -17,6 +19,24 @@ public class HasPathSum {
         }
         boolean left = root.left == null ? false : hasPathSum(root.left, sum - root.val);
         boolean right = root.right == null ? false :  hasPathSum(root.right, sum - root.val);
+        return left || right;
+    }
+}
+
+// Solution 2
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        return helper(root, sum);
+    }
+    public boolean helper(TreeNode root, int sum) {
+        if (root.val == sum && root.left == null && root.right == null) {
+            return true;
+        }
+        boolean left = root.left == null ? false : helper(root.left, sum - root.val);
+        boolean right = root.right == null ? false : helper(root.right, sum - root.val);
         return left || right;
     }
 }
